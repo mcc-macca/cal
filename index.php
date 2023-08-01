9a758456c0f18e4fd715d2f826a909c356e5f422
macca <mccsolo1942@gmail.com>
2023-08-01 16:26:08 +0200

    gg

--- index.php
+++ index.php
@@ -13,6 +13,8 @@ session_start();
 require("db.php");
 require "fun.php";
 
+$session_token = "OdnudFIn4iofwni4int4Vndi.Sovj9ej03irndlq";
+
 if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
     header("location: login.php");
     die;
@@ -35,6 +37,7 @@ if (isset($_POST['submit_giorno_top'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MCkalnedar</title>
     <link rel="stylesheet" href="style.css">
+    <script src="jquery.js"></script>
 </head>
 
 <body>
@@ -90,29 +93,135 @@ if (isset($_POST['submit_giorno_top'])) {
     </div>
     <div class="content">
         <?php
-            if (isset($_GET['viewmode'])) {
-
-            } else {
+        if (isset($_GET['viewmode'])) {
+            // SOMETHING!
+        } elseif (isset($_GET['action'])) {
+            $getaction = $_GET['action'];
+            if ($getaction == 'addevent') {
                 ?>
-                <table>
+                <h1>AGGIUNGI EVENTO AL DATABASE</h1>
+                <table class="modaltable">
                     <tr>
-                        <td rowspan="2" class="bordered">
-
-                        </td>
-                        <td class="bordered">
-
-                        </td>
+                        <td>Titolo:</td>
+                        <td colspan="2"><input type="text" name="titoloevento" placeholder="Nuovo Evento"></td>
                     </tr>
                     <tr>
-                        <td class="bordered">
-                            
+                        <td>Luogo:</td>
+                        <td colspan="2"><input type="text" name="luogoevento"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="3"><hr></td>
+                    </tr>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <td colspan="2"><input type="checkbox" name="alldayevento" id="alldayevento"> Evento di tutto il giorno</td>
+                    </tr>
+                    <tr>
+                        <td>Inizio:</td>
+                        <td><input type="date" name="datainizio" id="datainizio"></td>
+                        <td><input type="time" name="orainizio" id="orainizio"></td>
+                    </tr>
+                    <tr>
+                        <td>Fine:</td>
+                        <td><input type="date" name="datafine" id="datafine"></td>
+                        <td><input type="time" name="orafine" id="orafine"></td>
+                    </tr>
+                    <tr>
+                        <td colspan="3"><hr></td>
+                    </tr>
+                    <tr>
+                        <td colspan="3">Descrizione:</td>
+                    </tr>
+                    <tr>
+                        <td colspan="3">
+                            <textarea name="descrizioneEvento" id="descrizioneEvento" cols="30" rows="5"></textarea>
                         </td>
                     </tr>
                 </table>
                 <?php
             }
+        } else {
+        ?>
+            <table class="default">
+                <tr>
+                    <td rowspan="2" class="bordered">
+                        <h1>Prossimi eventi in calendario <a href="index.php?action=addevent"><button>+ AGGIUNGI EVENTO</button></a></h1>
+                        <?php
+                        $prossimieventiq = $conn->query("SELECT * FROM kal_eventi WHERE datainizio <= '" . date("d/m/Y") . "' AND caricatoda='" . $_SESSION['uid'] . "';");
+                        if ($prossimieventiq->num_rows > 0) {
+                            while ($pedt = $prossimieventiq->fetch_assoc()) {
+                                $x = $x + 1;
+                            }
+                        } else {
+                            print "<h2 style='color: #ff0000'>NO EVENTS PRESENT IN THE DATABASE</h2>";
+                        }
+                        ?>
+                    </td>
+                    <td class="bordered">
+                        <h1>Questa settimana</h1>
+                        <?php
+                        // Ottenere la data corrente
+                        $oggi = time();
+
+                        // Calcolare la data del lunedì della settimana corrente
+                        $lunedi_settimana_corrente = strtotime("monday", $oggi);
+
+                        // Generare la tabella
+                        echo '<table border="1">
+        <tr>
+            <th>GIORNO</th>
+            <th>EVENTI</th>
+        </tr>';
+
+                        // Ciclo per i giorni della settimana
+                        for ($i = 0; $i < 7; $i++) {
+                            // Calcolare la data del giorno corrente
+                            $giorno_corrente = strtotime("+$i days", $lunedi_settimana_corrente);
+
+                            // Formattare la data nel formato desiderato (es. "D d/m")
+                            $data_formattata = date("D d/m", $giorno_corrente);
+
+                            // Contenuto di esempio per gli eventi
+                            $evento = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima accusamus iste laudantium impedit culpa laborum harum ex! Unde animi aliquam laborum rerum, perferendis sapiente quo iure ipsa aspernatur pariatur reprehenderit!";
+
+                            // Stampa delle righe della tabella con giorno ed evento
+                            echo "<tr>
+            <td>$data_formattata</td>
+            <td>$evento</td>
+        </tr>";
+                        }
+
+                        echo '</table>';
+                        ?>
+
+                    </td>
+                </tr>
+                <tr>
+                    <td class="bordered">
+                        <h1>Ultimi aggiornamenti</h1>
+                    </td>
+                </tr>
+            </table>
+        <?php
+        }
         ?>
     </div>
+    <script>
+        $(document).ready(function() {
+            $("#alldayevento").click(function() {
+                // Controlla se il checkbox è selezionato o deselezionato
+                if ($(this).prop("checked")) {
+                    // Se è selezionato, disabilita gli input di ora
+                    $("#orainizio").prop("disabled", true);
+                    $("#orafine").prop("disabled", true);
+                } else {
+                    // Se è deselezionato, abilita gli input di ora
+                    $("#orainizio").prop("disabled", false);
+                    $("#orafine").prop("disabled", false);
+                }
+            });
+        });
+    </script>
 </body>
 
 </html>
